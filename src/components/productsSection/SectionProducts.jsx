6433bbcbbba7756c5c299a02c9d12f9450bc270a
Ailen <ailenfranco43@gmail.com>
2024-09-30 18:08:26 -0300

    Menu categorias y responsividad

--- src/components/productsSection/SectionProducts.jsx
+++ src/components/productsSection/SectionProducts.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import CategoriesNav from './categoriesNav/CategoriesNav.jsx'
+import styles from './SectionProducts.module.css'
 
 export const SectionProducts = () => {
   return (
-    <section id='productos'></section>
+    <section id='productos' className={styles.containerSectionProducts}>
+      <div className={styles.containerCategoriesNav}>
+
+        <CategoriesNav/>
+      </div>
+
+      
+
+    </section>
   )
 }
---
+++ src/components/productsSection/categoriesNav/CategoriesNav.jsx
@@ -0,0 +1,38 @@
+"use client"
+import React from 'react';
+import styles from './CategoriesNav.module.css';
+
+
+const CategoriesNav = () => {
+  return (
+<nav className={styles.categoriesNav}>
+    <ul className={styles.categoriesList}>
+        <li className={styles.categoryItem}>
+            <button className={styles.categoryLink}>Todos los productos</button>
+        </li>
+        <li className={styles.categoryItem}>
+            <button className={styles.categoryLink}>Productos de piscinas</button>
+        </li>
+        <li className={styles.categoryItem}>
+            <button className={styles.categoryLink}>Productos de invernadero</button>
+        </li>
+        <li className={styles.categoryItem}>
+            <button className={styles.categoryLink}>Productos de mediasombras</button>
+        </li>
+        <li className={styles.categoryItem}>
+            <button className={styles.categoryLink}>Telas no tejidas</button>
+        </li>
+        <li className={styles.categoryItem}>
+            <button className={styles.categoryLink}>Goma espuma</button>
+        </li>
+        <li className={styles.categoryItem}>
+            <button className={styles.categoryLink}>Artículos de ferretería</button>
+        </li>
+    </ul>
+</nav>
+
+
+  );
+};
+
+export default CategoriesNav;
