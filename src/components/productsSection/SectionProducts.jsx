885190d095dd8948c2c2c6fea781761da0bdd3b0
Ailen <ailenfranco43@gmail.com>
2024-10-07 22:54:41 -0300

    Carga de productos y categorias desde JSON

--- src/components/productsSection/SectionProducts.jsx
+++ src/components/productsSection/SectionProducts.jsx
@@ -5,52 +5,41 @@ import ProductCard from './productCard/ProductCard.jsx';
 import styles from './SectionProducts.module.css';
 
 const SectionProducts = () => {
-  // Definir productos directamente en el componente
-  const products = [
-    { id: 1, name: 'Nylon agrario', category: 'nylon', image: '/img/products/producto1.jpeg' , description: 'Nylon agrario negro ,especial para obra, se usa para hormigón,tapar inmuebles , viene en 2 3 4 6 y 8 metros de ancho en 100’y 200 micrones'},
-    { id: 2, name: 'Producto 2', category: 'nylon', price: 200 },
-    { id: 3, name: 'Producto 3', category: 'mediasombras', price: 300 },
-    { id: 4, name: 'Producto 4', category: 'ferreteria', price: 400 },
-    { id: 5, name: 'Producto 5', category: 'goma-espuma', price: 500 },
-    { id: 6, name: 'Producto 5', category: 'goma-espuma', price: 500 },
-    { id: 7, name: 'Producto 7', category: 'mediasombras', price: 300 },
-    { id: 8, name: 'Producto 8', category: 'ferreteria', price: 400 },
-    { id: 9, name: 'Producto 9', category: 'goma-espuma', price: 500 },
-    { id: 10, name: 'Producto 10', category: 'piscinas', price: 100 },
-    { id: 20, name: 'Producto 11', category: 'nylon', price: 200 },
-    { id: 30, name: 'Producto 12', category: 'mediasombras', price: 300 },
-    { id: 40, name: 'Producto 13', category: 'ferreteria', price: 400 },
-    { id: 50, name: 'Producto 14', category: 'goma-espuma', price: 500 },
-    { id: 60, name: 'Producto 15', category: 'goma-espuma', price: 500 },
-    { id: 70, name: 'Producto 16', category: 'mediasombras', price: 300 },
-    { id: 80, name: 'Producto 17', category: 'ferreteria', price: 400 },
-    { id: 90, name: 'Producto 18', category: 'goma-espuma', price: 500 },
-  ];
+  const [products, setProducts] = useState([]); // Inicialmente vacío
+  const [filteredProducts, setFilteredProducts] = useState([]); 
+  const [selectedCategory, setSelectedCategory] = useState('all'); 
+  const [categories, setCategories] = useState([]); // Estado para las categorías
 
+  // Función para obtener los productos desde el JSON
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('/data/products.json');
+      const data = await response.json();
+      setProducts(data);
+      setFilteredProducts(data); // Inicialmente mostrar todos los productos
+    };
 
-    // Objeto que mapea valores de categorías a nombres legibles
-  const categoryNames = {
-    all: 'Todos los productos',
-    piscinas: 'Productos de piscinas',
-    nylon: 'Nylon de invernadero',
-    mediasombras: 'Mediasombras',
-    'no-tejidas': 'Telas no tejidas',
-    'goma-espuma': 'Goma espuma',
-    ferreteria: 'Artículos de ferretería',
-    'cubre-cercos': 'Cubre cercos',
-  };
+    fetchProducts();
+  }, []);
 
-  const [filteredProducts, setFilteredProducts] = useState(products); // Inicializar con todos los productos
-  const [selectedCategory, setSelectedCategory] = useState("all"); // Categoria seleccionada por defecto
+  // Función para obtener las categorías desde el JSON
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const response = await fetch('/data/categories.json');
+      const data = await response.json();
+      setCategories(data); // Guardar las categorías en el estado
+    };
 
+    fetchCategories();
+  }, []);
 
   const handleSelectCategory = (category) => {
-    setSelectedCategory(category); // Actualizar la categoría seleccionada
-    if (category === "all") {
-      setFilteredProducts(products); // Mostrar todos los productos si se selecciona "Todos los productos"
+    setSelectedCategory(category);
+    if (category === 'all') {
+      setFilteredProducts(products);
     } else {
       const filtered = products.filter((product) => product.category === category);
-      setFilteredProducts(filtered); // Filtrar productos según la categoría seleccionada
+      setFilteredProducts(filtered);
     }
   };
 
@@ -58,34 +47,30 @@ const SectionProducts = () => {
     <section id='catalogo' className={styles.sectionProducts}>
       <h2 className={styles.sectionTitle}>Catálogo</h2>
       <div className={styles.containerSectionProducts}>
-
-
-      <div className={styles.containerCategoriesNav}>
-        <p>Filtrar por categoría:</p>
-        <CategoriesNav
-        onSelectCategory={handleSelectCategory} 
-         selectedCategory={selectedCategory} // Pasar la categoría seleccionada
-        />
-      </div>
-      <div className={styles.containerProducts}>
-
-        <div className={styles.containerProductsTitle}>
-          <h3 className={styles.productsTitle}>
-          {categoryNames[selectedCategory]}
-          </h3>
+        <div className={styles.containerCategoriesNav}>
+          <p>Filtrar por categoría:</p>
+          <CategoriesNav
+            categories={categories} // Pasar las categorías obtenidas al componente CategoriesNav
+            onSelectCategory={handleSelectCategory}
+            selectedCategory={selectedCategory}
+          />
         </div>
-        <div className={styles.containerCards}>
-          {filteredProducts.length > 0 ? (
-            filteredProducts.map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))
-          ) : (
-            <p>No hay productos disponibles en esta categoría.</p>
-          )}
+        <div className={styles.containerProducts}>
+          <div className={styles.containerProductsTitle}>
+            <h3 className={styles.productsTitle}>
+              {categories.find(cat => cat.value === selectedCategory)?.name || "Productos"}
+            </h3>
+          </div>
+          <div className={styles.containerCards}>
+            {filteredProducts.length > 0 ? (
+              filteredProducts.map((product) => (
+                <ProductCard key={product.id} product={product} />
+              ))
+            ) : (
+              <p>No hay productos disponibles en esta categoría.</p>
+            )}
+          </div>
         </div>
-
-      </div>
-
       </div>
     </section>
   );
--- src/components/productsSection/categoriesNav/CategoriesNav.jsx
+++ src/components/productsSection/categoriesNav/CategoriesNav.jsx
@@ -1,27 +1,55 @@
 
+// "use client";
+// import React from 'react';
+// import styles from './CategoriesNav.module.css';
+
+// const CategoriesNav = ({ onSelectCategory, selectedCategory  }) => {
+//   const categories = [
+//     { name: 'Todos los productos', value: 'all' },
+//     { name: 'Productos de piscinas', value: 'piscinas' },
+//     { name: 'Nylon de invernadero', value: 'nylon' },
+//     { name: 'Mediasombras', value: 'mediasombras' },
+//     { name: 'Telas no tejidas', value: 'no-tejidas' },
+//     { name: 'Goma espuma', value: 'goma-espuma' },
+//     { name: 'Artículos de ferretería', value: 'ferreteria' },
+//     { name: 'Cubre cercos', value: 'cubre-cercos' },
+//   ];
+
+//   return (
+//     <nav className={styles.categoriesNav}>
+//       <ul className={styles.categoriesList}>
+//         {categories.map(category => (
+//           <li className={styles.categoryItem} key={category.value}>
+//             <button
+//               className={`${styles.categoryLink} ${selectedCategory === category.value ? styles.active : ''}`} // Aplicar clase si está seleccionada
+//               onClick={() => onSelectCategory(category.value)}
+//             >
+//               {category.name}
+//             </button>
+//           </li>
+//         ))}
+//       </ul>
+//     </nav>
+//   );
+// };
+
+// export default CategoriesNav;
+
+
+
+
 "use client";
 import React from 'react';
 import styles from './CategoriesNav.module.css';
 
-const CategoriesNav = ({ onSelectCategory, selectedCategory  }) => {
-  const categories = [
-    { name: 'Todos los productos', value: 'all' },
-    { name: 'Productos de piscinas', value: 'piscinas' },
-    { name: 'Nylon de invernadero', value: 'nylon' },
-    { name: 'Mediasombras', value: 'mediasombras' },
-    { name: 'Telas no tejidas', value: 'no-tejidas' },
-    { name: 'Goma espuma', value: 'goma-espuma' },
-    { name: 'Artículos de ferretería', value: 'ferreteria' },
-    { name: 'Cubre cercos', value: 'cubre-cercos' },
-  ];
-
+const CategoriesNav = ({ onSelectCategory, selectedCategory, categories }) => {
   return (
     <nav className={styles.categoriesNav}>
       <ul className={styles.categoriesList}>
         {categories.map(category => (
           <li className={styles.categoryItem} key={category.value}>
             <button
-              className={`${styles.categoryLink} ${selectedCategory === category.value ? styles.active : ''}`} // Aplicar clase si está seleccionada
+              className={`${styles.categoryLink} ${selectedCategory === category.value ? styles.active : ''}`}
               onClick={() => onSelectCategory(category.value)}
             >
               {category.name}
@@ -34,6 +62,5 @@ const CategoriesNav = ({ onSelectCategory, selectedCategory  }) => {
 };
 
 export default CategoriesNav;
-
 
 
